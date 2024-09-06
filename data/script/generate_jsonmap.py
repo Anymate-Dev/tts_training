cb5f27030d1d68b774b2cbc1452e8a7547d00830
Max <max@any-mate.com>
2024-09-06 17:20:59 +0800

    update script

--- data/script/generate_jsonmap.py
+++ data/script/generate_jsonmap.py
@@ -18,19 +18,17 @@ def scan_directory(base_path):
             person_path = os.path.join(lang_path, person)
             if os.path.isdir(person_path):
                 catalog[lang][person] = {}
-                for emotion in os.listdir(person_path):
-                    emotion_path = os.path.join(person_path, emotion)
-                    if os.path.isdir(emotion_path):
-                        files_info = {}
-                        file_names = [os.path.splitext(f)[0] for f in os.listdir(emotion_path)]
-                        unique_file_names = set(file_names)
-                        for base_name in unique_file_names:
-                            wav_path = os.path.join(emotion_path, base_name + '.wav')
-                            lab_path = os.path.join(emotion_path, base_name + '.lab')
-                            if os.path.isfile(wav_path) and os.path.isfile(lab_path):
-                                text_content = read_text_file(lab_path)
-                                files_info[base_name] = {'file': wav_path, 'text': text_content}
-                        catalog[lang][person][emotion] = list(files_info.values())[:5]  # Only include up to 5 pairs
+
+                files_info = {}
+                file_names = [os.path.splitext(f)[0] for f in os.listdir(person_path)]
+                unique_file_names = set(file_names)
+                for base_name in unique_file_names:
+                    wav_path = os.path.join(person_path, base_name + '.wav')
+                    lab_path = os.path.join(person_path, base_name + '.lab')
+                    if os.path.isfile(wav_path) and os.path.isfile(lab_path):
+                        text_content = read_text_file(lab_path)
+                        files_info[base_name] = {'file': wav_path, 'text': text_content}
+                        catalog[lang][person] = list(files_info.values())[:5]  # Only include up to 5 pairs
     return catalog
 
 def write_catalog_to_json(catalog, output_file):
@@ -44,7 +42,7 @@ def write_catalog_to_json(catalog, output_file):
 
 
 
-base_directory = '/home/anymate/project/GPT-SoVITS/data/filter_data'
+base_directory = '/home/anymate/project/GPT-SoVITS/raw_data/filtered_data'
 catalog = scan_directory(base_directory)
 output_file = 'directory_catalog.json'
 write_catalog_to_json(catalog, output_file)
--- inference/server.py
+++ inference/server.py
@@ -1,52 +1,79 @@
 import gradio as gr
 import os
 import soundfile as sf
+import uuid  # Add this line
 from tools.i18n.i18n import I18nAuto
 from GPT_SoVITS.inference_webui import change_gpt_weights, change_sovits_weights, get_tts_wav
 
 i18n = I18nAuto()
 
-def synthesize(GPT_model_path, SoVITS_model_path, ref_audio_path, ref_text_content, ref_language, target_text_content, target_language, output_path):
-    # Change model weights
-    change_gpt_weights(gpt_path=GPT_model_path)
-    change_sovits_weights(sovits_path=SoVITS_model_path)
+def synthesize( ref_audio_path, ref_text_content, ref_language, target_text_content, target_language, output_path, how_to_cut):
 
-    # Synthesize audio
-    synthesis_result = get_tts_wav(ref_wav_path=ref_audio_path, 
-                                   prompt_text=ref_text_content, 
-                                   prompt_language=i18n(ref_language), 
-                                   text=target_text_content, 
-                                   text_language=i18n(target_language), top_p=1, temperature=1)
     
-    result_list = list(synthesis_result)
 
-    if result_list:
-        last_sampling_rate, last_audio_data = result_list[-1]
-        output_wav_path = os.path.join(output_path, "output.wav")
-        sf.write(output_wav_path, last_audio_data, last_sampling_rate)
-        return output_wav_path
+    GPT_model_path='/home/anymate/project/GPT-SoVITS/GPT_weights_v2/new-e25.ckpt'
+    SoVITS_model_path='/home/anymate/project/GPT-SoVITS/SoVITS_weights_v2/new_e8_s7664.pth'
+    
+    
+
+    try:
+        # Change model weights
+        change_gpt_weights(gpt_path=GPT_model_path)
+        change_sovits_weights(sovits_path=SoVITS_model_path)
+
+        # Synthesize audio
+        synthesis_result = get_tts_wav(ref_wav_path=ref_audio_path, 
+                                       prompt_text=ref_text_content, 
+                                       prompt_language=i18n(ref_language), 
+                                       text=target_text_content, 
+                                       text_language=i18n(target_language), 
+                                       top_k=20,top_p=0.8, temperature=0.8, how_to_cut=how_to_cut,
+                                       ref_free=False, speed=1, if_freeze=False, inp_refs=None
+                                       )
+        
+        result_list = list(synthesis_result)
+        os.makedirs(output_path, exist_ok=True)
+
+        if result_list:
+            last_sampling_rate, last_audio_data = result_list[-1]
+            unique_filename = f"output_{uuid.uuid4().hex[:8]}.wav"
+
+            output_wav_path = os.path.join(output_path, unique_filename)
+            print(f"Synthesis complete. Output path: {output_wav_path}")
+            sf.write(output_wav_path, last_audio_data, last_sampling_rate)
+            return output_wav_path
+        else:
+            print("Synthesis produced no results.")
+            return None
+    except Exception as e:
+        print(f"An error occurred during synthesis: {str(e)}")
+        return None
+
+def gradio_synthesize(ref_audio, ref_text, ref_language, target_text, target_language, how_to_cut):
+    output_path = "output"
+    os.makedirs(output_path, exist_ok=True)
+
+    output_wav_path = synthesize(ref_audio, ref_text, ref_language, target_text, target_language, output_path, how_to_cut)
+    return output_wav_path
 
 def main():
-    def gradio_synthesize(GPT_model_path, SoVITS_model_path, ref_audio, ref_text, ref_language, target_text, target_language):
-        output_path = "output"
-        os.makedirs(output_path, exist_ok=True)
-        ref_audio_path = ref_audio  # ref_audio is now a file path string
-        output_wav_path = synthesize(GPT_model_path, SoVITS_model_path, ref_audio_path, ref_text, ref_language, target_text, target_language, output_path)
-        return output_wav_path
-
     with gr.Blocks() as server:
         gr.Markdown("# GPT-SoVITS Web Interface")
-        GPT_model_path = gr.Textbox(label="Path to the GPT model file")
-        SoVITS_model_path = gr.Textbox(label="Path to the SoVITS model file")
+        
         ref_audio = gr.Textbox(label="Path to the Reference Audio File")
         ref_text = gr.TextArea(label="Reference Text")
         ref_language = gr.Dropdown(choices=["中文", "英文", "日文"], label="Reference Language")
         target_text = gr.TextArea(label="Target Text")
+        how_to_cut = gr.TextArea(label="How to cut")
         target_language = gr.Dropdown(choices=["中文", "英文", "日文", "中英混合", "日英混合", "多语种混合"], label="Target Language")
         output_audio = gr.Audio(label="Output Audio", type="filepath")
 
+
         synthesize_button = gr.Button("Synthesize")
-        synthesize_button.click(gradio_synthesize, inputs=[GPT_model_path, SoVITS_model_path, ref_audio, ref_text, ref_language, target_text, target_language], outputs=output_audio, concurrency_limit=16)
+        synthesize_button.click(gradio_synthesize, 
+                                inputs=[GPT_model_path, SoVITS_model_path, ref_audio, ref_text, ref_language, target_text, target_language, how_to_cut],
+                                outputs=output_audio, 
+                                concurrency_limit=16)
 
     server.launch(server_name="0.0.0.0", max_threads=16)
 
